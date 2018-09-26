af377af175ca5853efbe1e915f308bae5c1702ca
Rowell Duallo <rowellduallo@Rowells-MacBook-Air.local>
2018-09-26 23:18:30 +0800

    added commons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
+import { SideBarComponent } from './commons/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
