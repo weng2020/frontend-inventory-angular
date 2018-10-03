28ae62e10fe1a0c38225f09de24d9e40ba5e957e
Ritchie W <ritchie@evotechdev.com>
2018-10-03 14:54:22 +0800

    updated commons

--- src/app/pages/item-record/item.module.ts
+++ src/app/pages/item-record/item.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ItemService } from './item.service';
 import { ItemSearchComponent } from './search/search.component';
+import { CommonsModule } from '../../commons/commons.module';
 
 @NgModule({
   declarations: [
   ItemSearchComponent
 ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonsModule
   ],
   providers: [ItemService],
   exports:[
